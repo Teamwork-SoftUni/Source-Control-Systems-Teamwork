714c0ffb4b1db58fedf11974911a97596ac30fb1
MJordanov81 <marian.jordanov@gmail.com>
2017-10-16 22:42:29 +0300

    Task 3-2, Loading Advertisements

--- scripts/api.js
+++ scripts/api.js
@@ -1,5 +1,18 @@
 (function () {
 // Mock repository
+    let adverts = [
+        {
+            _id: 0,
+            _acl: {
+                creator: 0
+            },
+            title: "XBoss 1080",
+            publisher: "Pesho",
+            datePublished: "2017-06-04",
+            price: 100
+        }
+    ];
+
     let users = [
         {
             _kmd: {
@@ -26,6 +39,8 @@
             password: "m"
         }
     ];
+
+
 
     // User login
     $.mockjax(function (requestSettings) {
@@ -79,4 +94,22 @@
             };
         }
     });
+
+    // Loading of adverts
+    $.mockjax(function (requestSettings) {
+        if (requestSettings.url === "https://mock.api.com/appdata/kid_rk/adverts" &&
+            requestSettings.method === "GET") {
+            return {
+                response: function (origSettings) {
+                    if (requestSettings.headers["Authorization"].includes("Kinvey mock_token")) {
+                        this.responseText = adverts;
+                    } else {
+                        this.status = 403;
+                        this.responseText = "You are not authorized";
+                    }
+                }
+            };
+        }
+    });
 })();
+
