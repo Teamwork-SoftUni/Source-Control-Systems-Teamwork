e3ec091cb191e46331bf945aa31894936bf65ac3
BiserBorisov <butoff@abv.bg>
2017-10-16 21:09:38 +0300

    Stage1-Task2-Added-functions

--- scripts/prodavachnik.js
+++ scripts/prodavachnik.js
@@ -1,3 +1,28 @@
 function startApp() {
+    showView('viewHome');
 
+    // Bind the navigation menu links
+    $("#linkHome").click(showHomeView);
+    $("#linkLogin").click(showLoginView);
+    $("#linkRegister").click(showRegisterView);
+
+    function showView(viewName) {
+        // Hide all views and show the selected view only
+        $('main > section').hide();
+        $('#' + viewName).show();
+    }
+
+    function showHomeView() {
+        showView('viewHome');
+    }
+
+    function showLoginView() {
+        showView('viewLogin');
+        $('#formLogin').trigger('reset');
+    }
+
+    function showRegisterView() {
+        $('#formRegister').trigger('reset');
+        showView('viewRegister');
+    }
 }
