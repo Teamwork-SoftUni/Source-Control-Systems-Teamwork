5131702008e441ea6a21c38749fb96d6034f6413
Alisa Terzieva <alisa_terzieva@abv.bg>
2017-10-17 10:23:02 +0300

    Finished Task 4-1. Close #9

--- scripts/prodavachnik.js
+++ scripts/prodavachnik.js
@@ -1,4 +1,9 @@
 function startApp() {
+    if (sessionStorage.getItem('authToken') !== null) {
+        let username = sessionStorage.getItem('username');
+        $('#loggedInUser').text("Welcome, " + username + "!");
+    }
+
     showHideMenuLinks();
     showHomeView();
 
@@ -25,11 +30,13 @@ function startApp() {
             $("#linkLogin").show();
             $("#linkRegister").show();
             $("#linkLogout").hide();
+            $("#loggedInUser").hide();
         } else {
             // We have logged in user
             $("#linkLogin").hide();
             $("#linkRegister").hide();
             $("#linkLogout").show();
+            $("#loggedInUser").show();
         }
     }
 
@@ -78,6 +85,9 @@ function startApp() {
         sessionStorage.setItem('authToken', userAuth);
         let userId = userInfo._id;
         sessionStorage.setItem('userId', userId);
+        let username = userInfo.username;
+        sessionStorage.setItem('username', username);
+        $('#loggedInUser').text("Welcome, " + username + "!");
     }
 
     // user/register
